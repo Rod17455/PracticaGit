3e2f7f2b4b102c8f3bd02ea45a08f58459f50e82
Rodrigo Tellez <rtellez476@gmail.com>
2023-05-17 17:48:17 -0600

    Implemento de boton con función de mostrar imagen

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,17 @@ var button = document.getElementById("myButton");
 button.addEventListener("click", function() {
   alert("¡Hola! Has hecho clic en el botón.");
 });
+
+// JavaScript
+var showImageBtn = document.getElementById("showImageBtn");
+var myImage = document.getElementById("myImage");
+
+showImageBtn.addEventListener("click", function() {
+    if (myImage.style.display === "none") {
+        myImage.src = "kenny.jfif";
+        myImage.style.display = "block";
+      } else {
+        myImage.style.display = "none";
+      }
+});
+// 
